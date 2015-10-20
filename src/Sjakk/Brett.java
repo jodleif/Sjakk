230339ff12e1fa3ec3dfd653f38e9d3c45a768bf
Jo Øivind Gjernes <jodleif@gmail.com>
2015-10-20 13:04:15 +0200

    Spillbrettet.

---
+++ src/Sjakk/Brett.java
@@ -0,0 +1,29 @@
+package Sjakk;
+
+/**
+ * Created by Jo Øivind Gjernes on 20.10.2015.
+ */
+
+
+public class Brett
+{
+	private static final int BRETTSTØRRELSE = 8;
+	private int spillNr;
+	private Brikke[][] brikkene;
+
+	public Brett(int nyttSpillNr){
+
+	}
+
+	public static boolean erLovligRutenavn(String rutenavn){
+
+	}
+
+	public Brikke getBrikke(String rutenavn){
+
+	}
+
+	public boolean flyttBrikke{
+
+}
+}
